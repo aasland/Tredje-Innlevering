69c828eaeb2440317851eed5c00fd9c29be48015
Aasland <frederikaasland21@gmail.com>
2024-05-07 09:08:30 +0200

    img

---
+++ JAVASCRIPT/planetShooter.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", game);
+
+var sprite = new Image();
+var spriteExplosion = new Image();
+sprite.src = "../Bilder/allGFX.png";
+
+window.onload = function () {
+  spriteExplosion.src = "../Bilder/ExGFX.png";
+};
