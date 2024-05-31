b4456a161cf6b9a5fe2270447e90ded41a7e4c98
Aasland <frederikaasland21@gmail.com>
2024-05-31 19:48:29 +0200

    midlertidig løsning av filer

--- planetShooter.js
+++ planetShooter.js
@@ -2,10 +2,10 @@ window.addEventListener("DOMContentLoaded", game);
 
 var sprite = new Image();
 var spriteExplosion = new Image();
-sprite.src = "../Bilder/allGFX.png";
+sprite.src = "Bilder/allGFX.png";
 
 window.onload = function () {
-  spriteExplosion.src = "../Bilder/ExGFX.png";
+  spriteExplosion.src = "Bilder/ExGFX.png";
 };
 
 function game() {
